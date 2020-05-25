3646f4b824e3c878b143a072f9742803d4cf6a25
Aliaksei <mario.belarus@gmail.com>
2020-05-26 01:10:43 +0200

    homework 11.2 v2

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -3,13 +3,11 @@ import { Link } from 'react-router-dom'
 
 const Dashboard = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div
-        id="title"
-        className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10"
-      >
-        Dashboard
-      </div>
+    <div
+      id="title"
+      className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10"
+    >
+      Dashboard
       <div>
         <Link to="/dashboard/profile/9d8de08d-4893-46a5-8dfa-2008f7749500">Go To Profile</Link>
       </div>
--- client/components/profile.js
+++ client/components/profile.js
@@ -4,24 +4,22 @@ import { Link, useParams } from 'react-router-dom'
 const Profile = () => {
   const { username } = useParams()
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div
-        id="title"
-        className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10"
-      >
-        Profile
+    <div
+      id="title"
+      className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10"
+    >
+      Profile
+      <div>
+        <Link to="/dashboard/main">Go To Main</Link>
+      </div>
+      <div>
+        <Link to="/dashboard">Go to Root</Link>
       </div>
       <div
         id="username"
         className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10"
       >
         {username}
-      </div>
-      <div>
-        <Link to="/dashboard/main">Go To Main</Link>
-      </div>
-      <div>
-        <Link to="/dashboard">Go to Root</Link>
       </div>
     </div>
   )
